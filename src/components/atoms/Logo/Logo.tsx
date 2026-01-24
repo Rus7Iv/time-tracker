b8c6dbaf4d63c32f4c3523ad642fea19610bb08c
Ruslan Ivanov <rusivary@gmail.com>
2026-01-24 14:47:57 +0400

    feat/config | Added "alpha" version tag

--- src/components/atoms/Logo/Logo.tsx
+++ src/components/atoms/Logo/Logo.tsx
@@ -11,6 +11,8 @@ export const LogoWithText = ({ className }: IStyledLogoWithTextProps) => {
     <LogoContainer className={className}>
       <TimerAnimatedIcon />
       <LogoText>TimeTracker</LogoText>
+      {/* TODO: убрать в будущем */}
+      <AlphaTag>alpha</AlphaTag>
     </LogoContainer>
   )
 }
@@ -25,4 +27,16 @@ const LogoContainer = styled.div`
 const LogoText = styled.span`
   color: ${({ theme }) => theme.colors.navyblue};
   font-size: 24px;
+`
+
+const AlphaTag = styled.span`
+  color: ${({ theme }) => theme.colors.orange};
+  border: 1px solid ${({ theme }) => theme.colors.orange};
+  border-radius: 10px;
+  font-size: 10px;
+  font-weight: 600;
+  letter-spacing: 0.06em;
+  line-height: 1;
+  padding: 2px 6px;
+  text-transform: uppercase;
 `
